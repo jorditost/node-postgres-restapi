39b42fc133229be6ec1d43b7ff4fe14ad482381e
Jordi Tost <tost.jordi@gmail.com>
2016-07-11 19:05:18 +0200

    rows creation/update/removal

--- api/index.js
+++ api/index.js
@@ -3,9 +3,10 @@ var router = express.Router();
 
 var db = require('./queries');
 
-
 router.get('/api/places', db.getAllPlaces);
 router.get('/api/places/:id', db.getPlace);
-
+router.post('/api/places', db.createPlace);
+router.put('/api/places/:id', db.updatePlace);
+router.delete('/api/places/:id', db.removePlace);
 
 module.exports = router;
--- api/queries.js
+++ api/queries.js
@@ -10,14 +10,12 @@ var connectionString = 'postgres://localhost:5432/locations'; // Locations is an
 var db = pgp(connectionString);
 
 
+/////////////////////
 // Query Functions
-module.exports = {
-    getAllPlaces: getAllPlaces,
-    getPlace: getPlace
-};
+/////////////////////
 
 function getAllPlaces(req, res, next) {
-  db.any('select * from places_table')
+  db.any('select * from places')
     .then(function (data) {
       res.status(200)
         .json({
@@ -33,7 +31,7 @@ function getAllPlaces(req, res, next) {
 
 function getPlace(req, res, next) {
   var pupID = parseInt(req.params.id);
-  db.one('select * from places_table where gid = $1', pupID)
+  db.one('select * from places where id = $1', pupID)
     .then(function (data) {
       res.status(200)
         .json({
@@ -46,3 +44,66 @@ function getPlace(req, res, next) {
       return next(err);
     });
 }
+
+function createPuppy(req, res, next) {
+  req.body.age = parseInt(req.body.age);
+  db.none('insert into places(name, type, population)' +
+      'values(${name}, ${breed}, ${age}, ${sex})',
+    req.body)
+    .then(function () {
+      res.status(200)
+        .json({
+          status: 'success',
+          message: 'Inserted one place'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+function updatePuppy(req, res, next) {
+  db.none('update places set name=$1, type=$2, population=$3 where id=$4',
+    [req.body.name, req.body.type, parseInt(req.body.population),
+      parseInt(req.params.id)])
+    .then(function () {
+      res.status(200)
+        .json({
+          status: 'success',
+          message: 'Updated place'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+function removePuppy(req, res, next) {
+  var pupID = parseInt(req.params.id);
+  db.result('delete from places where id = $1', pupID)
+    .then(function (result) {
+      /* jshint ignore:start */
+      res.status(200)
+        .json({
+          status: 'success',
+          message: `Removed ${result.rowCount} place`
+        });
+      /* jshint ignore:end */
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+
+/////////////
+// Exports
+/////////////
+
+module.exports = {
+    getAllPlaces: getAllPlaces,
+    getPlace: getPlace,
+    createPlace: createPlace,
+    updatePlace: updatePlace,
+    removePlace: removePlace
+};
